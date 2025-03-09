cfbfcf0b8e60705aee85617d81180b8ef1147dac
Darko <darkoshypka@gmail.com>
2025-03-09 16:47:24 +0100

    Started Task2a

---
+++ list1/Task2a.java
@@ -0,0 +1,22 @@
+package list1;
+
+import java.awt.image.BufferedImage;
+
+public class Task2a {
+
+    public static void main(String[] args) {
+
+        int width = 500;
+        int height = 500;
+
+        int centerX;
+        int centerY;
+
+        BufferedImage image;
+
+        image = Utils.loadImage(args[0]);
+
+
+    }
+
+}
